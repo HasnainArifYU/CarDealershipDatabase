8a0d7ae36366a0c7f0bdb42273895da6b934344d
HasnainArifYU <harif@pgh.yearup.org>
2024-06-08 22:06:43 -0400

    createdatabase fixes

--- db_script.sql
+++ db_script.sql
@@ -14,24 +14,29 @@ CREATE TABLE Dealerships (
 CREATE TABLE Vehicles (
 	vehicle_id INT AUTO_INCREMENT PRIMARY KEY,
     VIN CHAR(17) NOT NULL,
-    Sold BOOLEAN NOT NULL
+    sold BOOLEAN NOT NULL
     );
     
 CREATE TABLE Inventory (
+	inventory_id INT AUTO_INCREMENT PRIMARY KEY,
+	dealership_id INT,
+    VIN CHAR(17),
 	FOREIGN KEY (dealership_id) REFERENCES Dealerships(dealership_id),
-    	FOREIGN KEY (VIN) REFERENCES Vehicles(VIN)
+	FOREIGN KEY (VIN) REFERENCES Vehicles(VIN)
         );
         
 CREATE TABLE Sales_Contracts (
 	sale_id INT AUTO_INCREMENT PRIMARY KEY,
+    VIN CHAR(17),
+    price FLOAT,
+    dateOfSale DATE,
     FOREIGN KEY (VIN) REFERENCES Vehicles(VIN)
     );
     
     
+-- Populate tables 
 
 
-    
-    
     
     
 
