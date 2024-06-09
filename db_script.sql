50925632ccdd83405f2ff3a660303d822dd148c1
HasnainArifYU <harif@pgh.yearup.org>
2024-06-08 22:21:05 -0400

    populate tables

--- db_script.sql
+++ db_script.sql
@@ -36,7 +36,29 @@ CREATE TABLE Sales_Contracts (
     
 -- Populate tables 
 
+INSERT INTO Dealerships (dealership_name, address, phone)
+VALUES ('Alpha Motors', '123 Main St', '555-1234'),
+       ('Irish Autos', '456 Oak Dr', '555-5678'),
+       ('Pittsburgh Cars', '789 River Rd', '555-9012');
 
+-- Insert sample data into the Vehicles table
+INSERT INTO Vehicles (VIN, sold)
+VALUES ('1HGCM82633A123456', FALSE),
+       ('2HGCM82633A654321', TRUE),
+       ('3HGCM82633A789012', FALSE),
+       ('4HGCM82633A345678', TRUE);
+
+-- Insert sample data into the Inventory table
+INSERT INTO Inventory (dealership_id, VIN)
+VALUES (1, '1HGCM82633A123456'),
+       (1, '2HGCM82633A654321'),
+       (2, '3HGCM82633A789012'),
+       (3, '4HGCM82633A345678');
+
+-- Insert sample data into the Sales_Contracts table
+INSERT INTO Sales_Contracts (VIN, price, dateOfSale)
+VALUES ('2HGCM82633A654321', 20000.00, '2023-05-15'),
+       ('4HGCM82633A345678', 22000.00, '2023-06-01');
     
     
 
