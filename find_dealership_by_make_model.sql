6672c740dcf13304f92bffee6b3a8370871a6b38
HasnainArifYU <harif@pgh.yearup.org>
2024-06-09 22:44:27 -0400

    minor fixes

--- find_dealership_by_make_model.sql
+++ find_dealership_by_make_model.sql
@@ -3,4 +3,5 @@ USE DealershipDatabase;
 SELECT d.dealership_name, d.address, d.phone
 FROM Dealerships d
 JOIN Inventory i ON d.dealership_id = i.dealership_id
-WHERE i.VIN = '1HGCM82633A123456';
+JOIN Vehicles v ON i.VIN = v.VIN
+WHERE v.make = 'Honda' AND v.model = 'Civic' AND v.color = 'Blue';
