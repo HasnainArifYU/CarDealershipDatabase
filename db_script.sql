1af91e2746b389289d31b81f38ba0b66c69398df
HasnainArifYU <harif@pgh.yearup.org>
2024-06-09 22:38:31 -0400

    added a color column to vehicles

--- db_script.sql
+++ db_script.sql
@@ -16,6 +16,7 @@ CREATE TABLE Vehicles (
     make VARCHAR(15) NOT NULL,
     model VARCHAR(20) NOT NULL,
     production_year INT NOT NULL,
+    color VARCHAR(10) NOT NULL,
     VIN CHAR(17) NOT NULL UNIQUE,
     sold BOOLEAN NOT NULL
 );
@@ -36,28 +37,28 @@ CREATE TABLE Sales_Contracts (
     FOREIGN KEY (VIN) REFERENCES Vehicles(VIN)
 );
 
--- Populate tables 
+-- Populate tables
 
 INSERT INTO Dealerships (dealership_name, address, phone)
 VALUES ('Alpha Motors', '123 Main St', '555-1234'),
        ('Irish Autos', '456 Oak Dr', '555-5678'),
        ('Pittsburgh Cars', '789 River Rd', '555-9012');
 
--- Insert sample data into the Vehicles table
-INSERT INTO Vehicles (make, model, production_year, VIN, sold)
-VALUES ('Honda', 'Accord', 2003, '1HGCM82633A123456', FALSE),
-       ('Honda', 'Civic', 2004, '2HGCM82633A654321', TRUE),
-       ('Toyota', 'Camry', 2005, '3HGCM82633A789012', FALSE),
-       ('Ford', 'Focus', 2006, '4HGCM82633A345678', TRUE);
 
--- Insert sample data into the Inventory table
+INSERT INTO Vehicles (make, model, production_year, color, VIN, sold)
+VALUES ('Honda', 'Accord', 2003, 'Black', '1HGCM82633A123456', FALSE),
+       ('Honda', 'Civic', 2004, 'Blue', '2HGCM82633A654321', TRUE),
+       ('Toyota', 'Camry', 2005, 'Red', '3HGCM82633A789012', FALSE),
+       ('Ford', 'Focus', 2006, 'White', '4HGCM82633A345678', TRUE);
+
+
 INSERT INTO Inventory (dealership_id, VIN)
 VALUES (1, '1HGCM82633A123456'),
        (1, '2HGCM82633A654321'),
        (2, '3HGCM82633A789012'),
        (3, '4HGCM82633A345678');
 
--- Insert sample data into the Sales_Contracts table
+
 INSERT INTO Sales_Contracts (VIN, price, dateOfSale)
 VALUES ('2HGCM82633A654321', 20000.00, '2023-05-15'),
        ('4HGCM82633A345678', 22000.00, '2023-06-01');
